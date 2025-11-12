258bbf8969f88b47c49231a9be67fba34577090d
Livia Magalhães <liviamagalhaes344@gmail.com>
2025-11-11 21:58:02 -0300

    Add files via upload

---
+++ pyyy/main.py
@@ -0,0 +1,23 @@
+import modulo 
+
+def moda(notas):
+    moda1 = modulo.mode(notas)
+    return moda1
+
+def media(notas):
+    media1 = modulo.mean(notas)
+    return media1
+
+
+def mediana(notas):
+    mediana1 = modulo.median(notas)
+    return mediana1
+
+def desvio(notas):
+    desvio1 = modulo.stdev(notas)
+    return desvio1
+
+
+
+
+
---
+++ pyyy/modulo.py
@@ -0,0 +1,31 @@
+def verificar():
+
+    print('Notas escolares: ')
+    notas = [9, 10, 1, 9.6, 8, 6, 4]
+
+    print('media')
+    media1 = (notas)
+    print(media1)
+
+    print('mediana')
+    mediana1 = (notas)
+    print(mediana1)
+
+    print('moda')
+    moda1 = (notas)
+    print(moda1)
+
+    print('desvio')
+    desvio1 = (notas)
+    print(desvio1)
+
+
+verificar()
+    
+
+
+
+
+
+
+
